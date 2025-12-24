703dac99d6ea4eb29114fb0ce00d65afcf69c576
elwhyjay <lyj4032@naver.com>
2025-12-25 06:08:30 +0900

    feat: LLamaRMSNorm 클래스에 타입 힌트 추가 및 정규화 함수 리팩토링

--- model/layer_norm.py
+++ model/layer_norm.py
@@ -1,14 +1,18 @@
 import torch
 import torch.nn as nn
+import math
 
 class LLamaRMSNorm(nn.Module):
-    def __init__(self,hidden_size, eps = 1e-6,bias=True):
+    def __init__(self,hidden_size: int , eps = 1e-6,bias=True):
         super().__init__()
         self.hidden_size = hidden_size
         self.eps = eps
         self.weight = nn.Parameter(torch.ones(hidden_size))
         self.bias = nn.Parameter(torch.zeros(hidden_size))
         self.isbias = bias
+
+    def norm(self,x):
+        return torch.rsqrt(torch.mean(x ** 2, dim=-1, keepdim=True) + self.eps)
 
     def forward(self, x):
         """
@@ -22,10 +26,7 @@ class LLamaRMSNorm(nn.Module):
         x: 인풋 텐서 (batch_size, seq_len, hidden_size)
         return 정규화된 텐서
         """
-        variance = torch.mean(x ** 2, dim=-1, keepdim=True)
-        x = x * (self.weight / torch.sqrt(variance + self.eps))
-        x *= self.weight
-        return x
+        output = self.norm(x.float()).type_as(x)
     
-
+        return output*self.weight
     
